11473a6bf770751f3217fa9293f2cd4a567c2110
Chimpy Dev <85179880+chimpyswap@users.noreply.github.com>
2021-06-12 21:56:03 +0100

    Initial commit
    Add Sdk to exchange.chimpyswap.com

---
+++ test/constants.test.ts
@@ -0,0 +1,20 @@
+import { INIT_CODE_HASH } from '../src/constants'
+
+//import { bytecode } from '@uniswap/v2-core/build/UniswapV2Pair.json'
+//import { keccak256 } from '@ethersproject/solidity'
+
+// this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
+// and load the JSON.
+//const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+
+describe('constants', () => {
+  describe('INIT_CODE_HASH', () => {
+    // it('matches computed bytecode hash', () => {
+    //   expect(COMPUTED_INIT_CODE_HASH).toEqual(INIT_CODE_HASH)
+    // })
+    //TODO: Replace with real ChimpyPair.json
+    it('matches computed bytecode hash', () => {
+      expect(INIT_CODE_HASH).toEqual('0xe289d6ffd036691cd9065bdc6297a74c54ead088b1b145840a304afadf0a6c7b')
+    })
+  })
+})
